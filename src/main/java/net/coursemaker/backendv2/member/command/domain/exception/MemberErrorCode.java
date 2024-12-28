88a0c3f11357a4dff47f796f8af737c84b72d3e4
Isaiah IM <46448947+isaiahIM@users.noreply.github.com>
2024-12-28 23:22:49 +0900

    CM-23 feat: JWT 발행로직 개발

--- src/main/java/net/coursemaker/backendv2/member/command/domain/exception/MemberErrorCode.java
+++ src/main/java/net/coursemaker/backendv2/member/command/domain/exception/MemberErrorCode.java
@@ -10,8 +10,8 @@ import lombok.Getter;
 public enum MemberErrorCode implements ErrorCode {
 
 	EMAIL_DUPLICATED("USER-001", "duplicated data", HttpStatus.BAD_REQUEST),
-	NICKNAME_DUPLICATED("USER-002", "duplicated data", HttpStatus.BAD_REQUEST),
-	PHONE_NUMBER_DUPLICATED("USER-003", "duplicated data", HttpStatus.BAD_REQUEST),
+	NICKNAME_DUPLICATED("USER-002", "duplicated data", HttpStatus.CONFLICT),
+	PHONE_NUMBER_DUPLICATED("USER-003", "duplicated data", HttpStatus.CONFLICT),
 	MEMBER_NOT_FOUND("USER-004", "user not found", HttpStatus.NOT_FOUND),
 	PASSWORD_NOT_CORRECT("USER-005", "password not correct", HttpStatus.BAD_REQUEST),
 	EMAIL_VERIFICATION_NOT_SEND("USER-006", "verification code not send", HttpStatus.BAD_REQUEST),
